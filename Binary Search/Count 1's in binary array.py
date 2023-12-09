5c92e615f659a12d61f40106275e3af1b680f096
IamTypeToxic <madhav2004cbe@gmail.com>
2023-12-09 23:45:10 +0530

    question to be discussed with mam

---
+++ Binary Search/Count 1's in binary array.py
@@ -0,0 +1,23 @@
+
+x = 1
+
+def Binary_Search(n,arr,x):
+    high = n-1
+    low = 0
+    high = n-1
+    counter = 0
+    while arr[0] >= x > arr[-1]:
+        mid = low + (high - low)//2
+        if arr[mid] == x:
+            counter+=1
+            arr.pop(mid)  #can also use remove in this place
+        elif arr[mid] < x:
+            high = mid - 1
+    return counter
+
+T = int(input())
+for i in range(T):
+    n = int(input())
+    arr = list(map(int,input().split()))
+    ans = Binary_Search(n,arr,x)
+    print(ans)
